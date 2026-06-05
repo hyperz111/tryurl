1f9c83bf06dda832b75467aab3c89f28846eb2b8
hyperz111 <firzeroun11@gmail.com>
2026-06-05 22:17:44 +0700

    refactor(create): use individual url input

--- src/routes/create/+page.svelte
+++ src/routes/create/+page.svelte
@@ -1,11 +1,33 @@
 <script>
 	let { form } = $props();
+
+	let urls = $state([""]);
+	let value = $derived(urls.join("\n"));
+
+	const remove = (index) => {
+		if (urls.length > 1) {
+			urls[index] = null;
+			urls = urls.filter((url) => url !== null);
+		}
+	};
 </script>
 
 <h1>Create</h1>
 
 <form method="POST">
-	<textarea name="urls" placeholder="One URL per line" required></textarea>
+	<!--- Concat all URLs in here --->
+	<textarea name="urls" bind:value hidden></textarea>
+
+	<div>
+		{#each urls as url, index}
+			<span>
+				<input type="text" bind:value={urls[index]} required />
+				<button onclick={() => remove(index)}>×</button>
+			</span>
+		{/each}
+	</div>
+
+	<button onclick={() => urls.push("")}>Add</button>
 	<button type="submit">Create</button>
 </form>
 
