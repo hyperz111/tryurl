ed74d950d15ab992377f61f772920fc055ded704
hyperz111 <firzeroun11@gmail.com>
2026-06-06 18:15:59 +0700

    feat: add navigation bar

---
+++ src/lib/NavigationBar.svelte
@@ -0,0 +1,44 @@
+<script>
+	import { onNavigate } from "$app/navigation";
+	import { slide } from "svelte/transition";
+
+	let opened = $state(false);
+
+	const menus = [
+		["/", "Home"],
+		["/create", "Create"],
+		["/edit", "Edit"],
+	];
+
+	onNavigate(() => {
+		opened = false;
+	});
+</script>
+
+<header class="menu">
+	<nav class="navbar has-shadow">
+		<div class="navbar-brand">
+			<a href="/" class="navbar-item">TryURL</a>
+			<button title="navbar burger button" class="navbar-burger" onclick={() => (opened = !opened)}>
+				<span></span>
+				<span></span>
+				<span></span>
+				<span></span>
+			</button>
+		</div>
+		<div class="navbar-menu">
+			<div class="navbar-end">
+				{#each menus as [href, label] (href)}
+					<a {href} class="navbar-item">{label}</a>
+				{/each}
+			</div>
+		</div>
+	</nav>
+	{#if opened}
+		<div transition:slide class="menu-list">
+			{#each menus as [href, label] (href)}
+				<a {href} class="menu-item">{label}</a>
+			{/each}
+		</div>
+	{/if}
+</header>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script>
 	import "bulma/css/versions/bulma-no-dark-mode.css";
 	import favicon from "$lib/assets/favicon.svg";
+	import NavigationBar from "$lib/NavigationBar.svelte";
 
 	let { children } = $props();
 </script>
@@ -8,6 +9,8 @@
 <svelte:head>
 	<link rel="icon" href={favicon} />
 </svelte:head>
+
+<NavigationBar />
 
 <main class="container is-fluid">
 	{@render children()}
