e2241e46963109d46031186e75cc0131f1b10c36
hyperz111 <firzeroun11@gmail.com>
2026-09-04 19:32:51 +0700

    chore: no condition for a meanwhile

--- src/lib/storage.js
+++ src/lib/storage.js
@@ -1,35 +1,13 @@
-const stores = new Map();
+import { getStore } from "@netlify/blobs";
 
-let getStorage = (name) => {
-	if (!stores.has(name)) {
-		const store = new Map();
-		const object = {
-			get: async (key) => store.get(key),
-			set: async (key, value) => store.set(key, value),
-			delete: async (key) => store.delete(key),
-		};
+const getStorage = (name) => {
+	const store = getStore({ name });
 
-		stores.set(name, object);
-	}
-
-	return stores.get(name);
-};
-
-if (import.meta.env.PROD) {
-	const { getStore } = await import("@netlify/blobs");
-	getStorage = (name) => {
-		const store = getStore({
-			name,
-			siteID: process.env.NETLIFY_SITE_ID,
-			token: process.env.NETLIFY_TOKEN,
-		});
-
-		return {
-			get: (key) => store.get(key, { type: "json" }),
-			set: (key, value) => store.setJSON(key, value),
-			delete: (key) => store.delete(key),
-		};
+	return {
+		get: (key) => store.get(key, { type: "json" }),
+		set: (key, value) => store.setJSON(key, value),
+		delete: (key) => store.delete(key),
 	};
-}
+};
 
 export default getStorage;
